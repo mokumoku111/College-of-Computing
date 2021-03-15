5c442334190b68ef031f8bbc634e789efd24af5e
mokumoku111 <aphiwat_p@kkumail.com>
2021-03-15 21:16:15 +0700

    add folder dockerCms to github

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-15 13:47:24 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-15T13:47:23+00:00	INFO 172.19.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
